01521416577324f617e724808d8672d2504654b3
Régis dos anjos <regisanjos47@gmail.com>
2024-06-18 16:13:54 -0300

    implementando routers

--- src/router/routerDeliveryman.js
+++ src/router/routerDeliveryman.js
@@ -1,1 +1,15 @@
-const
+const express = require('express');
+const DeliverymanController = require('../controllers/deliverymanController');
+// Importe middlewares
+
+const router = express.Router();
+
+
+router.post('/', createDeliverymanValidation, DeliverymanController.create);
+router.get('/',  , DeliverymanController.getAll);
+router.get('/:id', ,DeliverymanController.getById);
+router.put('/:id', ,DeliverymanController.update);
+router.delete('/:id', , DeliverymanController.delete);
+
+
+module.exports = router;
