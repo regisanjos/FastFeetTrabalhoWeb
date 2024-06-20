fccac9d83699de4743b1c1eac1edabad89229789
regis <regisanjos47@gmail.com>
2024-06-19 21:18:18 -0300

    limpado comentario

--- src/router/routerRecipient.js
+++ src/router/routerRecipient.js
@@ -5,4 +5,3 @@ const RecipientController = require('../controllers/recipientController');
 router.post('/recipients', RecipientController.create);
 router.put('/recipients/:id', RecipientController.update);
 router.delete('/recipients/:id', RecipientController.delete);
-// Adicione outras rotas para recipients conforme necessário
